edd3d3030efbe92282c2ee27e124f3b94c282491
Seth Nelson <seth.nelson@outlook.com>
2020-01-26 18:45:32 -0800

    started setup of tabs component and setup API/error call

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,18 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios
+    .get('https://lambda-times-backend.herokuapp.com/topics')
+    .then(response => {
+        response.data.topics.forEach(element => {
+            topicsContainer.appendChild(tabComponent(element));
+        })
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
+
+    
